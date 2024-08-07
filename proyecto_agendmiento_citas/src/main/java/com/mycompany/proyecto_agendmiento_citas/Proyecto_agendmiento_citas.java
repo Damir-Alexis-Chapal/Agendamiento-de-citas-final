1c9c621503f71847cdd438e355645fe875219cd5
unknown <dchapalimbaquin2018@gmail.com>
2024-08-07 10:28:43 -0500

    se creo el proyecto en netbeans

---
+++ proyecto_agendmiento_citas/src/main/java/com/mycompany/proyecto_agendmiento_citas/Proyecto_agendmiento_citas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_agendmiento_citas;
+
+/**
+ *
+ * @author Alexis Chapal
+ */
+public class Proyecto_agendmiento_citas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
